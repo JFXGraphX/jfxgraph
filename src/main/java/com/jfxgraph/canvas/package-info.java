f4d5f25ff2925f0944d1863a749dc02093e7d98e
Albert Sun <albert.suen2012@gmail.com>
2013-09-07 15:19:03 +0800

    init

---
+++ src/main/java/com/jfxgraph/canvas/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2012-2013,Epic-HUST Technology(Wuhan)Co.,Ltd. All Rights Reserved.
+ */
+/**
+ * 〈一句话功能简述〉
+ * <p>
+ * 〈功能详细描述〉
+ * </p>
+ * 
+ * @author Albert
+ * @version %I%, %G%
+ * @since 2013年9月7日 下午12:53:09 Albert .
+ */
+package com.jfxgraph.canvas;
